98104ebdca9eb2ca8845deb8c7e69f7fd23d4d49
guilherme-mota <motaguilherme99@ua.pt>
2022-03-01 16:30:03 +0000

    Extrair a localização do Gazebo

--- p_gmota_core/src/model_states_to_tf.py
+++ p_gmota_core/src/model_states_to_tf.py
@@ -16,17 +16,38 @@ from gazebo_msgs.msg import ModelState, ModelStates, ContactsState
 
 
 def callbackMsgReceived(models):
-    rospy.loginfo('Received Model States Message! ')
+    print('Received Model States Message!')
 
     # Get Game Parameters
     redTeam = rospy.get_param('/red_players')
     greenTeam = rospy.get_param('/green_players')
     blueTeam = rospy.get_param('/blue_players')
 
+    br = tf2_ros.TransformBroadcaster()
+
     for player in redTeam + greenTeam + blueTeam:
+        i = 0
         for name in models.name:
             if player == name:
                 print(name)
+
+                # Create and Populate my Transformation
+                t = geometry_msgs.msg.TransformStamped()
+                t.header.stamp = rospy.Time.now()
+                t.header.frame_id = 'world'
+                t.child_frame_id = '/' + name + '/odom'
+                t.transform.translation.x = models.pose[i].position.x
+                t.transform.translation.y = models.pose[i].position.y
+                t.transform.translation.z = models.pose[i].position.z
+                t.transform.rotation.x = models.pose[i].orientation.x
+                t.transform.rotation.y = models.pose[i].orientation.y
+                t.transform.rotation.z = models.pose[i].orientation.z
+                t.transform.rotation.w = models.pose[i].orientation.w
+
+                # Send Transformation
+                br.sendTransform(t)
+
+            i += 1
 
 
 def main():
