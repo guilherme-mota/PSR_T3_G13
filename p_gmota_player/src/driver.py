6031b9501f087452f3f386a0e4f4f3b5eef76f63
ornelas15 <david.ornelas@ua.pt>
2022-03-02 23:31:38 +0000

    "driver changes"

--- p_gmota_player/src/driver.py
+++ p_gmota_player/src/driver.py
@@ -11,6 +11,7 @@
 #   IMPORTS
 # ------------------------
 import sys
+import time
 import random
 import numpy as np
 import cv2
@@ -53,19 +54,28 @@ class Driver:
         greenTeam = rospy.get_param('/green_players')
         blueTeam = rospy.get_param('/blue_players')
 
-        # Print Player Information
+        # Print Player Information and assing their team and states
         if (self.name == redTeam[0]) or (self.name == redTeam[1]) or (self.name == redTeam[2]):
             print('Player team is Red')
             print('Player ' + self.name + ' hunts ' + str(greenTeam) + ' and runs from ' + str(blueTeam))
+            self.team = "RED"
             self.prey = "GREEN"
+            self.hunter = "BLUE"
+            self.state = "hunt"
         elif (self.name == greenTeam[0]) or (self.name == greenTeam[1]) or (self.name == greenTeam[2]):
             print('Player team is Green')
             print('Player ' + self.name + ' hunts ' + str(blueTeam) + ' and runs from ' + str(redTeam))
+            self.team = "GREEN"
             self.prey = "BLUE"
+            self.hunter = "RED"
+            self.state = "hunt"
         elif (self.name == blueTeam[0]) or (self.name == blueTeam[1]) or (self.name == blueTeam[2]):
             print('Player team is Blue')
             print('Player ' + self.name + ' hunts ' + str(redTeam) + ' and runs from ' + str(greenTeam))
+            self.team = "BLUE"
             self.prey = "RED"
+            self.hunter = "GREEN"
+            self.state = "hunt"
 
         self.publisher_command = rospy.Publisher('/' + self.name + '/cmd_vel', Twist, queue_size=1)
 
@@ -86,98 +96,147 @@ class Driver:
     def callback(self, data):
         global show_windows
 
+        #Convert image to openCV
         cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")  # cv_image type: numpy.ndarray
+        cv_image_g = copy.deepcopy(cv_image)
+        
+        #Convert to grey scale
+        cv_grey = cv2.cvtColor(cv_image_g, cv2.COLOR_BGR2GRAY)
 
-        if self.prey == "GREEN":
-            mask = cv2.inRange(cv_image, (0, 100, 0), (0, 255, 0))
-        elif self.prey == "RED":
-            mask = cv2.inRange(cv_image, (0, 0, 0), (0, 0, 255))
-        elif self.prey == "BLUE":
-            mask = cv2.inRange(cv_image, (0, 0, 0), (255, 0, 0))
+        # Image processing for Red team
+        if self.team == "RED" and self.prey == "GREEN" and self.hunter == "BLUE":
+            prey_cv_image = copy.deepcopy(cv_image)
+            hunter_cv_image = copy.deepcopy(cv_image)
+            prey_mask = cv2.inRange(prey_cv_image, (0, 100, 0), (0, 255, 0)) #green team
+            hunter_mask = cv2.inRange(hunter_cv_image, (0, 0, 0), (255, 0, 0)) #blue team
 
-        image_processed = copy.deepcopy(cv_image)
-        image_processed[np.logical_not(mask)] = 0
-
+        # Image processing for Blue team:
+        elif self.team == "BLUE" and self.prey == "RED" and self.hunter == "GREEN":
+            prey_cv_image = copy.deepcopy(cv_image)
+            hunter_cv_image = copy.deepcopy(cv_image)
+            prey_mask = cv2.inRange(prey_cv_image, (0, 0, 0), (0, 0, 255)) #red team
+            hunter_mask = cv2.inRange(hunter_cv_image, (0, 100, 0), (0, 255, 0)) #green team
+        
+        # Image processing for Green team:
+        elif self.team == "GREEN" and self.prey == "BLUE" and self.hunter == "RED":
+            prey_cv_image = copy.deepcopy(cv_image)
+            hunter_cv_image = copy.deepcopy(cv_image)
+            prey_mask = cv2.inRange(prey_cv_image, (0, 0, 0), (255, 0, 0)) #blue team
+            hunter_mask = cv2.inRange(hunter_cv_image, (0, 0, 0), (0, 0, 255)) #red team
+            
+        #Image processing for prey and hunter mask:
+        #Colored Mask
+        prey_img_processed = copy.deepcopy(cv_image)
+        hunter_img_processed = copy.deepcopy(cv_image)
+        prey_img_processed[np.logical_not(prey_mask)] = 0
+        hunter_img_processed[np.logical_not(hunter_mask)] = 0
+        
+        #Kernel 5x5 filter
         kernel = np.ones((5, 5), np.uint8)
-        img_dilation = cv2.dilate(image_processed, kernel, iterations=2)
 
-        if show_windows == "true":
-            cv2.imshow("Mask", mask)
-            cv2.imshow("Image Processed", image_processed)
-            cv2.imshow("Image Dilated", img_dilation)
+        #Dilation from both masks 
+        prey_img_dilation = cv2.dilate(prey_img_processed, kernel, iterations=2)
+        hunter_img_dilation = cv2.dilate(hunter_img_processed, kernel, iterations=2)
+          
+        #Convert both masks to grey img:
+        prey_image_grey = cv2.cvtColor(prey_img_dilation, cv2.COLOR_BGR2GRAY)
+        hunter_image_grey = cv2.cvtColor(hunter_img_dilation, cv2.COLOR_BGR2GRAY)
+        
+        #Thresholding from grey image:
+        _, prey_thresh = cv2.threshold(prey_image_grey, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        _, hunter_thresh = cv2.threshold(hunter_image_grey, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        
+        #Merge Prey with Hunter Mask:
+        ph_img = cv2.add(prey_img_dilation, hunter_img_dilation)
 
-        # Get Object
-        image_grey = cv2.cvtColor(image_processed, cv2.COLOR_BGR2GRAY)
-        _, thresh = cv2.threshold(image_grey, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh, 4, cv2.CV_32S)
-
+        #Get connected components from threshold:
+        prey_num_labels, prey_labels, prey_stats, prey_centroids = cv2.connectedComponentsWithStats(prey_thresh, 4, cv2.CV_32S)
+        hunter_num_labels, hunter_labels, hunter_stats, hunter_centroids = cv2.connectedComponentsWithStats(hunter_thresh, 4, cv2.CV_32S)
+        
+        
+        #Compare Max Area from Prey and Hunter Mask
+        #Decide if state is hunting or running
         try:
             # Get Object Max Area Centroid
-            max_area = 0
-            max_area_Label = None
-            for i in range(num_labels):
-                if i != 0 and max_area < stats[i, cv2.CC_STAT_AREA]:
-                    max_area = stats[i, cv2.CC_STAT_AREA]
-                    max_area_Label = i
+            prey_max_area = 0
+            hunter_max_area = 0
+            prey_max_area_Label = None
+            hunter_max_area_Label = None
+            
+            for i in range(prey_num_labels):
 
-            if max_area_Label is not None:
-                mask2 = cv2.inRange(labels, max_area_Label, max_area_Label)
-                mask2 = mask2.astype(bool)
-                if self.prey == "GREEN":
-                    cv_image[mask2] = (0, 255, 0)
-                elif self.prey == "RED":
-                    cv_image[mask2] = (0, 0, 255)
-                elif self.prey == "BLUE":
-                    cv_image[mask2] = (255, 0, 0)
+                if i != 0 and prey_max_area < prey_stats[i, cv2.CC_STAT_AREA]:
+                    p_x = prey_stats[i, cv2.CC_STAT_LEFT]
+                    p_y = prey_stats[i, cv2.CC_STAT_TOP]
+                    p_w = prey_stats[i, cv2.CC_STAT_WIDTH]
+                    p_h = prey_stats[i, cv2.CC_STAT_HEIGHT]
+                    p_area = prey_stats[i, cv2.CC_STAT_AREA]
+                    prey_max_area_Label = i
 
-                # Draw Line on Centroid
-                global x_last, y_last
-                x = int(centroids[max_area_Label, 0])
-                y = int(centroids[max_area_Label, 1])
-                if x_last is not None and y_last is not None:
-                    # Cross On Centroid
-                    cv2.line(cv_image, (x, y), (x_last + 5, y_last), (0, 0, 255), 1, cv2.LINE_4)
-                    cv2.line(cv_image, (x, y), (x_last - 5, y_last), (0, 0, 255), 1, cv2.LINE_4)
-                    cv2.line(cv_image, (x, y), (x_last, y_last + 5), (0, 0, 255), 1, cv2.LINE_4)
-                    cv2.line(cv_image, (x, y), (x_last, y_last - 5), (0, 0, 255), 1, cv2.LINE_4)
-                x_last = x
-                y_last = y
+                    if p_area > 10:
+                        global x_last, y_last
+                        to_delete = []
+                        exists = False
+                        (cX, cY) = prey_centroids[i]
+                        cX, cY = int(cX), int(cY)
+                        rect_img = cv2.rectangle(prey_thresh, (p_x, p_y), (p_x + p_w, p_y + p_h), (255, 255, 0), -1)
+                         
+                        # Draw Line on Centroid
+                        x = int(prey_centroids[prey_max_area_Label, 0])
+                        y = int(prey_centroids[prey_max_area_Label, 1])
+                        if x_last is not None and y_last is not None:
+                            # Cross On Centroid
+                            cv2.line(rect_img, (x, y), (x_last + 5, y_last), (0, 0, 255), 1, cv2.LINE_4)
+                            cv2.line(rect_img, (x, y), (x_last - 5, y_last), (0, 0, 255), 1, cv2.LINE_4)
+                            cv2.line(rect_img, (x, y), (x_last, y_last + 5), (0, 0, 255), 1, cv2.LINE_4)
+                            cv2.line(rect_img, (x, y), (x_last, y_last - 5), (0, 0, 255), 1, cv2.LINE_4)
+                            cv2.imshow("Rect img", rect_img)
+                        x_last = x
+                        y_last = y
+                        height, width, _ = cv_image.shape
 
-                height, width, _ = cv_image.shape
+                if prey_max_area_Label is not None:
+                    mask2 = cv2.inRange(prey_labels, prey_max_area_Label, prey_max_area_Label)
+                    mask2 = mask2.astype(bool)
+                    cv_image2 = copy.deepcopy(cv_image)
+                    if self.prey == "GREEN":
+                        cv_image2[mask2] = (0, 255, 0)
+                    elif self.prey == "RED":
+                        cv_image2[mask2] = (0, 0, 255)
+                    elif self.prey == "BLUE":
+                        cv_image2[mask2] = (255, 0, 0)
+              
+            print(x)
+            #Hunting
+            if x == 150:
+                self.angle = 0
+            elif x > 150:
+                self.angle = -1.5
+            elif x < 150:
+                self.angle = 1.5
 
-                # if x == 150:
-                #     self.angle = 0
-                # elif 150 < x < 175:
-                #     self.angle = -0.15
-                # elif 175 < x < 225:
-                #     self.angle = -0.3
-                # elif x > 225:
-                #     self.angle = -0.6
-                # elif 125 < x < 150:
-                #     self.angle = 0.15
-                # elif 75 < x < 125:
-                #     self.angle = 0.3
-                # elif x < 75:
-                #     self.angle = 0.6
-                if x == 150:
-                    self.angle = 0
-                elif x > 150:
-                    self.angle = -1.5
-                elif x < 150:
-                    self.angle = 1.5
-
-                # Publish position of the target
-                twist = Twist()
-                twist.linear.x = 0.75
-                twist.angular.z = self.angle
-                self.publisher_command.publish(twist)
+            # Publish position of the target
+            twist = Twist()
+            twist.linear.x = 0.75
+            twist.angular.z = self.angle
+            self.publisher_command.publish(twist)
         finally:
-            print("No player detected")
+            # print("No player detected")
+            pass
 
+        #Show image processing in output
         if show_windows == "true":
-            cv2.imshow("Camera Image", cv_image)
-
+            #cv2.imshow("Prey Mask", prey_mask)
+            #cv2.imshow("Prey Image Processed", prey_img_processed)
+            cv2.imshow("Prey Image Dilated", prey_img_dilation)
+            cv2.imshow("Prey-Hunter image", ph_img)
+            #cv2.imshow("Hunter Mask", hunter_mask)
+            #cv2.imshow("Hunter Image Processed", hunter_img_processed)
+            #cv2.imshow("Hunter Image Dilated", hunter_img_dilation)
+            
+            #cv2.imshow("Camera Image", cv_image)
         cv2.waitKey(3)
+
 
     def goalReceivedCallback(self, msg):
         print('Received New Goal on Frame ID ' + msg.header.frame_id)
